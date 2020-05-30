17d6c735025d1fd0008a78d6fce3d928fcc58581
Victor Jarvis <vctrjrvs@gmail.com>
2020-05-29 23:25:29 -0400

    Other initial commits and brainstorming

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//Main focus and objective: A hub for musicians to share and promote 
+//music/their brand etc, 
+
+//Feature List:
+//Header
+//Navigation Bar
+//Footer
+//Upload Forms - Audio and Video
+//Artist, Fan, and Record Label pages
+//Merchandise/Purchase Pages
+//Policy Page - Privacy, Copyright, Terms Of Service/Use
+//'Concert Hall' - Live Streams and Uploads of Live Performances
+//Forum for discussion, tips, etc
+//Upload Configurator? So everything can be probably set upon inital upload
+//Favorites feature for liked songs, albums, etc
+//Wish List for music, products, etc
+//Lesson/Instruction/Tutorial Pages?
+
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*content*/}
     </div>
   );
 }
-
-export default App;
